95f79655bb25022994391e99f55dfcd918bdd55b
gary.pang@gmail.com <gary.pang@gmail.com>
2024-02-17 15:02:27 -0500

    API Assignment 01 - Remove boilerplate instructions

--- voter-api/main.go
+++ voter-api/main.go
@@ -19,23 +19,6 @@ var (
 )
 
 // processCmdLineFlags parses the command line flags for our CLI
-//
-// TODO: This function uses the flag package to parse the command line
-//		 flags.  The flag package is not very flexible and can lead to
-//		 some confusing code.
-
-//			 REQUIRED:     Study the code below, and make sure you understand
-//						   how it works.  Go online and readup on how the
-//						   flag package works.  Then, write a nice comment
-//				  		   block to document this function that highights that
-//						   you understand how it works.
-//
-//			 EXTRA CREDIT: The best CLI and command line processor for
-//						   go is called Cobra.  Refactor this function to
-//						   use it.  See github.com/spf13/cobra for information
-//						   on how to use it.
-//
-//	 YOUR ANSWER: <GOES HERE>
 func processCmdLineFlags() {
 
 	//Note some networking lingo, some frameworks start the server on localhost
